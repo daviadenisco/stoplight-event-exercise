c07fbf4e25eeef1bab0dbe597cd6c0a799f383cf
Davia DeNisco <davia622@gmail.com>
2017-11-15 15:40:32 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,4 +2,26 @@
   'use strict';
 
   // YOUR CODE HERE
+  var stopButton = document.getElementById("stopButton");
+  var slowButton = document.getElementById("slowButton");
+  var goButton = document.getElementById("goButton");
+  var trafficLight = document.getElementById("stopLight");
+  var slowLight = document.getElementById("slowLight");
+  var goLight = document.getElementById("goLight");
+
+  stopButton.addEventListener("click", function () {
+    trafficLight.classList.toggle("stop");
+    // if (stopButton:hover) {
+    //   console.log("Entered stopButton button.")
+    // }
+  });
+
+  slowButton.addEventListener("click", function () {
+    slowLight.classList.toggle("slow");
+  });
+
+  goButton.addEventListener("click", function () {
+    goLight.classList.toggle("go");
+  });
+
 })();
